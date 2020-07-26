7dcbf0187759cecc1e3deaf221ee3051a8dd0b64
Tousif Chowdhury <tousif@Tousifs-MacBook-Pro.local>
2020-07-26 19:35:44 -0400

    need to get search to show up

--- client/src/Component/Search/Search.js
+++ client/src/Component/Search/Search.js
@@ -3,24 +3,38 @@ import React, { Component } from 'react';
 class Search extends Component { 
     constructor(props){
       super(props);
-
+    
       this.state = {token:props.token}
     }
     componentDidMount() {
-      fetch('http://localhost/search')
+      const data = {access_token:this.state.token, search_input:"testing this"};
+      console.log(this.state.token)
+      const url = `http://localhost:8888?access_token=${encodeURIComponent(data.access_token)}&search_input=${encodeURIComponent(data.search_input)}`
+      console.log(url)
+      fetch(url, {
+        method: 'GET',
+        mode: 'no-cors'
+      })
       .then(res => res.json())
       .then((data) => {
-        this.setState({ contacts: data })
-      })
-      .catch(console.log)
+        console.log(data)
+        this.setState({ search: data })
+      }).catch(console.log);
     }
 
     render() {
       return (
-        <h1></h1>
+      <h1>HI</h1>
+      
       )
     }
   }
+  /*
+fetch(`https://api.parse.com/1/users?foo=${encodeURIComponent(data.foo)}&bar=${encodeURIComponent(data.bar)}`, {
+  method: "GET",
+  headers: headers,   
+})
+  */
 
   /*
    document.getElementById('search-playlists').addEventListener('search', function(){
